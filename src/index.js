6d9269ee950693eeea767e7a5dbe19c1ffcda9c9
Ryan Park <ryan.developer99@gmail.com>
2021-04-07 21:51:20 +0900

    Fourth commit: BookList components has been created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-function Greeting() {
-  return <h1>HELLO</h1>;
+// css
+import "./index.css";
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+      alt=""
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>I Love You to the Moon and Back</h1>;
+};
+
+const Author = () => {
+  return <h4>Amelia Hepworth</h4>;
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
